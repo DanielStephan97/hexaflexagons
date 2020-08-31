c4957daeb491978f3691e7b93bfc95ea27503b7b
Niko Hastrich <niko.hastrich@student.hpi.uni-potsdam.de>
2020-08-31 20:40:14 +0200

    fixed stress test for modular arithmetic

--- stress-tests/number-theory/ModularArithmetic.cpp
+++ stress-tests/number-theory/ModularArithmetic.cpp
@@ -2,19 +2,41 @@
 
 #include "../../content/number-theory/ModularArithmetic.h"
 
+const int modprime = 257, modnonprime = 259;
+using ModPrime = TMod<modprime>;
+using ModNonPrime = TMod<modnonprime>;
 int main() {
-	rep(a,0,mod) rep(b,1,mod) {
-		Mod ma(a);
-		Mod mb(b);
-		Mod mc = ma / mb;
-		assert((mc * mb).x == a);
+	{
+		rep(a,0,modprime) rep(b,1,modprime) {
+			ModPrime ma(a), mb(b);
+			auto mc = ma / mb;
+			assert((mc * mb).x == a);
+		}
+		ModPrime a = 2;
+		ll cur=1;
+		rep(i, 0, modprime - 1) {
+			assert((a.pow(i)).x == cur);
+			cur = (cur * 2) % modprime;
+			// cout << i << ": " << (a ^ i).x << endl;
+		}
 	}
-	Mod a = 2;
-	ll cur=1;
-	rep(i, 0, 17) {
-		assert((a ^ i).x == cur);
-		cur = (cur * 2) % mod;
-		// cout << i << ": " << (a ^ i).x << endl;
+
+
+	{
+		rep(a,0,modnonprime) rep(b,1,modnonprime) {
+			if (max(gcd(a, modnonprime), gcd(b,modnonprime)) == 1) {
+				ModNonPrime ma(a), mb(b);
+				auto mc = ma / mb;
+				assert((mc * mb).x == a);
+			}
+		}
+		ModNonPrime a = 2;
+		ll cur=1;
+		rep(i, 0, modnonprime - 1) {
+			assert((a.pow(i)).x == cur);
+			cur = (cur * 2) % modnonprime;
+			// cout << i << ": " << (a ^ i).x << endl;
+		}
 	}
 	cout<<"Tests passed!"<<endl;
 }
