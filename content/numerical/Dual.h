a704590fbb9d88d6a724b5184093a6d651adcd33
Niko <niko.hastrich@student.hpi.uni-potsdam.de>
2021-01-08 21:18:18 +0100

    change dual

--- content/numerical/Dual.h
+++ content/numerical/Dual.h
@@ -7,17 +7,16 @@
  * \begin{align*}
  * 	\text{Primal:} \max v=&b^Tx &\text{Dual:} \min u=c^Ty\\
  * 	s.t.\quad x\gtreqless& 0& s.t.\quad y\gtreqless&0\\
- * 	Ax \gtreqless& c&A^Ty\gtreqless&b\\
+ * 	Ax \gtreqless& c&A^Ty\gtreqless&b
  * \end{align*}
  *
  * The dual of a linear program has the same optimum. If one of the programs is unbound the other is infeasible.
  * Each primal constraint becomes a dual variable, each primal variable becomes a dual constraint.
- * Relations might flip. Only take dual of maximization problem (which can be obtained from
- * minimization by maximizing $-b^Tx$). This can sometimes be useful to transform a problem to a flow problem.
  *
+ * \begin{center}
  * \begin{tabular}{|c|c||c|c|}
  * \hline
- * PRIMAL& maximize & minimize & DUAL\\\hline
+ * & maximize & minimize &\\\hline
  *  & $\leq b_i$ & $\geq 0$ &\\
  * constraint & $\geq b_i$ & $\leq 0$ & variable\\
  * & $=b_i$& unconstraint&\\
@@ -25,6 +24,9 @@
  * variable& $\leq 0$& $\leq c_j$&constraint\\
  * &$unconstraint$& $=c_j$&\\\hline
  * \end{tabular}
- *~\\
+ * \end{center}
+ *
+ * If in an optimal dual solution a variable in non zero then its primal constraint holds with equality.
+ * To get dual solution to MCMF use a SSP implementation then pi is the dual solution.
  */
 
